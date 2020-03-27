84d6041cc29e75c67552a3d21489335f387510d8
Кондратьев Егор Александрович <e.a.kondratiev@nes.lan>
2020-03-27 16:00:05 +0300

    'bootstrap'

--- 4. Fourth_week (web)/server.py
+++ 4. Fourth_week (web)/server.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = 'Прогноз погоды'
+    title = 'Новости Python'
     weather = weather_by_city("Moscow,Russia")
     return render_template('index.html', page_title = title, weather = weather)
 
 if __name__ == "__main__":
     app.run(debug = True)
 
+ 
